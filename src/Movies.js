334a78499ff2b78fa85fa06f3764f222558c8215
Bikram Prasad <bikram.prasad2017@vitstudent.ac.in>
2023-12-30 03:05:38 +0530

    added a check for img and search value in Not found component

--- src/Movies.js
+++ src/Movies.js
@@ -1,15 +1,25 @@
 import Movie from './Movie';
 import NotFound from './NotFound';
-const Movies = ({ movies }) => {
+const Movies = ({ movies, searchText }) => {
   return (
     <div className='container'>
       <div id='movie' className='row'>
         {movies && movies.length > 0 ? (
-          movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+          movies.map((movie) => (
+            <Movie
+              key={movie.imdbID}
+              {...movie}
+              Poster={
+                movie.Poster === 'N/A'
+                  ? 'https://dummyimage.com/300/000000/fff.png&text=No+Image+Available'
+                  : movie.Poster
+              }
+            />
+          ))
         ) : (
           <div>
             {' '}
-            <NotFound />
+            <NotFound searchText = {searchText}/>
           </div>
         )}
       </div>
--- src/NotFound.js
+++ src/NotFound.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const NotFound = ({}) => {
+const NotFound = ({searchText}) => {
   return (
-    <div className='col-md-3'>
-      <h1 align='center'>Not Found! </h1>
+    <div className='col-md'>
+      <h2 align='center'>No movies with the name <span style={{ fontWeight: 'bold', color:'red' }}>{searchText}</span> were found !</h2>
     </div>
   );
 };
